aff822174061a733a3ba5258e8befe9ac312c55c
Erica Nafziger and Taylor Loftis-Kim <erica@>
2016-10-18 13:25:45 -0700

    add jQ scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,23 @@
 $(document).ready(function(){
-  $("panel").click(function(){
-    $("")
-  })
+  $("#card1 h2").click(function(){
+    //$("#card1 h2").toggle();
+    $("#card1 .panel-body").toggle();
   });
-});
+  $("#card2 h2").click(function(){
+    //$("#card1 h2").toggle();
+    $("#card2 .panel-body").toggle();
+  });
+  $("#card3 h2").click(function(){
+    //$("#card1 h2").toggle();
+    $("#card3 .panel-body").toggle();
+  });
+
+
+
+
+
+
+
+
+
+  });
