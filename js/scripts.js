afdeb856e61e7623085f62c2d31de54840105e04
Erica Nafziger and Taylor Loftis-Kim <erica@>
2016-10-18 14:01:41 -0700

    fix jQuery tags so cards successfully toggle on and off

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,82 @@
 $(document).ready(function(){
-  $("#card1 h2").click(function(){
-    //$("#card1 h2").toggle();
+  $("#card1").click(function(){
+    $("#card1 h2").toggle();
     $("#card1 .panel-body").toggle();
   });
-  $("#card2 h2").click(function(){
-    //$("#card1 h2").toggle();
+  $("#card2").click(function(){
+    $("#card2 h2").toggle();
     $("#card2 .panel-body").toggle();
   });
-  $("#card3 h2").click(function(){
-    //$("#card1 h2").toggle();
+  $("#card3").click(function(){
+    $("#card3 h2").toggle();
     $("#card3 .panel-body").toggle();
   });
-
-
-
-
-
-
-
-
-
+  $("#card4").click(function(){
+    $("#card4 h2").toggle();
+    $("#card4 .panel-body").toggle();
+  });
+  $("#card5").click(function(){
+    $("#card5 h2").toggle();
+    $("#card5 .panel-body").toggle();
+  });
+  $("#card6").click(function(){
+    $("#card6 h2").toggle();
+    $("#card6 .panel-body").toggle();
+  });
+  $("#card7").click(function(){
+    $("#card7 h2").toggle();
+    $("#card7 .panel-body").toggle();
+  });
+  $("#card8").click(function(){
+    $("#card8 h2").toggle();
+    $("#card8 .panel-body").toggle();
+  });
+  $("#card9").click(function(){
+    $("#card9 h2").toggle();
+    $("#card9 .panel-body").toggle();
+  });
+  $("#card10").click(function(){
+    $("#card10 h2").toggle();
+    $("#card10 .panel-body").toggle();
+  });
+  $("#card11").click(function(){
+    $("#card11 h2").toggle();
+    $("#card11 .panel-body").toggle();
+  });
+  $("#card12").click(function(){
+    $("#card12 h2").toggle();
+    $("#card12 .panel-body").toggle();
+  });
+  $("#card13").click(function(){
+    $("#card13 h2").toggle();
+    $("#card13 .panel-body").toggle();
+  });
+  $("#card14").click(function(){
+    $("#card14 h2").toggle();
+    $("#card14 .panel-body").toggle();
+  });
+  $("#card15").click(function(){
+    $("#card15 h2").toggle();
+    $("#card15 .panel-body").toggle();
+  });
+  $("#card16").click(function(){
+    $("#card16 h2").toggle();
+    $("#card16 .panel-body").toggle();
+  });
+  $("#card17").click(function(){
+    $("#card17 h2").toggle();
+    $("#card17 .panel-body").toggle();
+  });
+  $("#card18").click(function(){
+    $("#card18 h2").toggle();
+    $("#card18 .panel-body").toggle();
+  });
+  $("#card19").click(function(){
+    $("#card19 h2").toggle();
+    $("#card19 .panel-body").toggle();
+  });
+  $("#card20").click(function(){
+    $("#card20 h2").toggle();
+    $("#card20 .panel-body").toggle();
+  });
   });
